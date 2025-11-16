5e3c681011ebe848d14f0003d6551cfffe0f8405
Christian Guetnga <lsdrfrx@gmail.com>
2025-11-16 15:15:35 +0300

    refactor: поправил ширину контента в MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,9 @@
     <VAppbar />
     <div class="inner">
       <VSidebar />
-      <slot />
+      <div class="content">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
@@ -23,5 +25,10 @@ import { VSidebar } from '@/components/sidebar'
   display: flex;
   height: calc(100vh - 6.4rem);
   gap: 2rem;
+}
+
+.content {
+  overflow: auto;
+  width: 100%;
 }
 </style>
