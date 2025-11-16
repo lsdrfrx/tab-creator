8d4a103711536fac111ca94dd5df75d94616fea1
Christian Guetnga <lsdrfrx@gmail.com>
2025-11-16 15:10:00 +0300

    feat: переделал компонент кнопки

--- src/components/button/VButton.vue
+++ src/components/button/VButton.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="btn" @click="$emit('click')">{{ text }}</div>
+  <div
+    class="btn"
+    :class="{ [variant]: variant, iconBtn: iconId && !text }"
+    @click="$emit('click')"
+  >
+    <template v-if="text">
+      {{ text }}
+    </template>
+    <template v-if="iconId">
+      <VIcon class="icon" :iconId />
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { VIcon } from '../icon'
+
+type Variant = 'filled' | 'ghost'
+
 interface Props {
-  text: string
-  variant?: string
+  text?: string
+  variant?: Variant
   size?: string
+  iconId?: string
 }
 
 const { text, variant = 'filled', size = 'M' } = defineProps<Props>()
@@ -18,21 +34,59 @@ defineEmits<{
 <style scoped lang="scss">
 .btn {
   display: flex;
-  background-color: var(--button-filled-bg);
-  color: var(--button-filled-fg);
   border-radius: 0.5rem;
   padding: 0.8rem;
   height: 4rem;
+  width: fit-content;
   justify-content: center;
   align-items: center;
   transition: all ease-in 0.2s;
+  gap: 0.8rem;
+  cursor: pointer;
 
-  &:hover {
-    background-color: var(--button-filled-hover);
+  &.iconBtn {
+    width: fit-content;
+    height: fit-content;
   }
 
-  &:active {
-    background-color: var(--button-filled-click);
+  &.filled {
+    background-color: var(--color-btn-bg);
+    color: var(--color-btn-text);
+
+    .icon {
+      color: var(--color-btn-text);
+    }
+
+    &:hover {
+      background-color: var(--color-btn-hover-bg);
+    }
+
+    &:active {
+      background-color: var(--color-accent);
+    }
+  }
+
+  &.ghost {
+    background-color: var(--color-bg);
+    color: var(--color-text);
+
+    .icon {
+      color: var(--color-accent);
+    }
+
+    &:hover {
+      background-color: var(--color-bg-hover);
+
+      .icon {
+        color: var(--color-accent-hover);
+      }
+    }
+
+    &:active {
+      .icon {
+        color: var(--color-accent-active);
+      }
+    }
   }
 }
 </style>
