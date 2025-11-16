75428f74ca82d16807345a47a3bfa6c673c94db8
Christian Guetnga <lsdrfrx@gmail.com>
2025-11-16 15:09:36 +0300

    feat: добавил компонент иконки

---
+++ src/components/icon/VIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <VueFeather class="icon" :type="iconId" />
+</template>
+
+<script setup lang="ts">
+import VueFeather from 'vue-feather'
+
+interface Props {
+  iconId: string
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+.icon {
+  width: 1.6rem;
+  height: 1.6rem;
+  transition: all ease-in 0.2s;
+}
+</style>
